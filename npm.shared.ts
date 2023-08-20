a33fa29bf5f7880a57153651cefd47d563811275
mainframenzo <67568925+mainframenzo@users.noreply.github.com>
2023-08-20 15:19:16 -0700

    WIP

--- npm.shared.ts
+++ npm.shared.ts
@@ -1,12 +1,15 @@
-// This file is responsible for defining shared dependencies across all packages.
+// This file is responsible for defining shared dependencies across all packages,
+//  including the package this is copied from, _ts-app-devkit.
 // It gets copied into all TSAPP packages during setup.
-// You can run `make packages/install` after updating this file to update downstream dependencies for TSAPP packages.
+// You can run `make packages/install` after updating this file in the _ts-app-devkit package 
+//  to update downstream shared dependencies for TSAPP packages.
 const npmPackage: any = {
   dependencies: {},
   devDependencies: {
     'typescript': '4.8.4',
     '@types/node': '18.11.18',
     'ts-node': '10.9.1',
+    'tslib': '2.4.0',
 
     // Cross-platform build support:
     'shx': '0.3.4', // Used to invoke cross-platform build commands.
